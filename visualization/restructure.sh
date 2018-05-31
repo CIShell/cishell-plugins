d75dcd90ccaeaa83bfbbeef7aeccf37d3ba9f757
varun-mach <vmaching@iu.edu>
2018-05-31 12:29:24 -0400

    bnd plugins and scripts added

---
+++ visualization/restructure.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+shopt -s extglob
+for f in */ 
+do
+  if [[ -d $f ]] 
+    then
+    cd $f
+    targetCount=$(find -maxdepth 1 -name "target" -type d | wc -l)
+    if [[ $targetCount -eq 1 ]]
+    then
+      srcCount=$(find -maxdepth 1 -name "src" -type d | wc -l)
+      if [[ $srcCount -eq 1 ]]
+      then
+        sed -i 's/<packaging>eclipse-plugin<\/packaging>//' pom.xml
+        mkdir -p src/main/java
+        mkdir -p src/main/resources
+        mv src/* src/main/java
+        metaCount=$(find -maxdepth 1 -name "META-INF" -type d | wc -l)
+        if [[ $metaCount -eq 1 ]]
+        then
+          mv META-INF src/main/resources
+        fi
+        osgiCount=$(find -maxdepth 1 -name "OSGI-INF" -type d | wc -l)
+        if [[ $osgiCount -eq 1 ]]
+        then
+          mv OSGI-INF src/main/resources
+        fi  
+      fi 
+    fi
+    # for f1 in */
+    # do
+    #   if [[ -d $f1 ]] 
+    #   then
+    #     echo $f1
+
+    #     if [[ $f1 = "target/" ]]
+    #       then
+    #         mkdir -p src/main/java
+    #         mkdir -p src/main/resources
+    #         mv src/* src/main/java
+    #         mv 
+    #       break
+    #     fi
+    #   fi   
+    # done
+    cd ..
+  fi
+done
+
+
